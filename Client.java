16c23e845c5e9fe7c06856505d8a0683e6556607
UmaKanaujia <77092190+UmaKanaujia@users.noreply.github.com>
2021-01-20 20:33:02 +0530

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,57 +1,5 @@
-package com.ui;
-import com.exceptions.*;
-import com.services.*;
+package com.capgemini.users;
 
-public class Client 
-{
-	public static void main(String[] args)
-	{
-		 BankInterface bank=new AxisBank();
-		try 
-		{
-			System.out.println("Creating a new Account");
-			System.out.println(bank.createAccount(101,5000));
-			System.out.println(bank.createAccount(102,10000));
-		}
-		catch(insufficientOpeningAmtException ioa) {
-			System.out.println("Insufficient amount for opening  account. Minimum balance must be 500");
-		}
-		try
-		{
-			System.out.println("Withdraw Amount");
-			System.out.println("Balance of account no 101 is: "+bank.withdrawAmount(101, 5000));
-		}
-		catch(insufficientBalanceException inb)
-		{
-			System.out.println("Insufficient Account Balance");
-		}
-		catch(invalidAccNumException ina)
-		{
-			System.out.println("Invalid Account Number");
-		}
-		try 
-		{
-			System.out.println("Deposit Amount");
-			System.out.println("Amount in account no 102 is : "+bank.depositAmount(102,7000));
-		}
-		catch(invalidAccNumException ina)
-		{
-			System.out.println("Invalid Account Number");
-		}
-		try 
-		{
-			System.out.println("Fund Transfer");
-			int []a=bank.fundTransfer(101, 102, 2000);
-			System.out.println("New balance of account number 101: "+a[0]+"  New balance of account number 102 : "+a[1]);
-		}
-		catch(insufficientBalanceException inb) 
-		{
-			System.out.println("Insufficient Balance");
-		}
-		catch(invalidAccNumException ina)
-		{
-			System.out.println("Invalid Account Number");
-		}
-	}
+public class Client {
 
 }
--- HDFCBank.java
+++ HDFCBank.java
@@ -1,5 +1,5 @@
 package com.capgemini.services;
 
-public class BankInterface {
+public class HDFCBank {
 
 }
